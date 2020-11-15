8d73793fe037da9718b3053353219842d41e45b3
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-15 21:37:29 +0100

    add caclculator as function component

--- src/components/Calculator/CalculatorFunction.js
+++ src/components/Calculator/CalculatorFunction.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import {Button, Input, Select, Result} from './components';
+import { Button, Input, Select, Result } from './components';
 
-
+const VAT = 0.23;
 
 function CalculatorFunction() {
+    const [amount, setAmount] = useState('');
+    const [tip, setTip] = useState('');
+    const [result, setResult] = useState(0)
+
+    const handleAmountChange = (event) => {
+        const value = event.target.value;
+        setAmount(value);
+    }
+
+    const handleTipChange = (event) => {
+        const value = event.target.value;
+        setTip(value);
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+        if (amount !== 0 && tip !== 0) {
+            let tipAmount = parseFloat(amount) * parseFloat(tip);
+            let resultWithoutTax = parseFloat(amount) + tipAmount;
+            let taxAmount = resultWithoutTax * VAT;
+            let result = resultWithoutTax + taxAmount;
+
+            setResult(Math.round(result * 100) / 100);
+        }
+
+    };
     return (
         <form onSubmit={handleSubmit}>
             <h2>Calculator (func)</h2>
-            <Input value={amount} changeHandler={handleAmountChange}/>
+            <Input value={amount} changeHandler={handleAmountChange} />
             <Select value={tip} changeHandler={handleTipChange} />
             <Button />
             <Result value={result} />
@@ -16,4 +45,5 @@ function CalculatorFunction() {
 
     );
 }
+
 export default CalculatorFunction;
